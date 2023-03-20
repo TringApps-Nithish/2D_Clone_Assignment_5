c598d53e57839096473bb1776b9bb76b9290d2fe
TringApps-Nithish <nithiya.prasath@tringapps.com>
2023-03-20 11:58:32 +0530

    done

--- src/main/java/assiginandcheck/AssignPoint.java
+++ src/main/java/assiginandcheck/AssignPoint.java
@@ -5,12 +5,12 @@ import java.util.logging.Logger;
 
 public class AssignPoint extends CloneNotSupportedException implements Cloneable {
     protected Logger l = Logger.getLogger("GatherInfo");
-    protected int xAxis1;
-    protected int yAxis1;
+    private final int xAxis1;
+    private final int yAxis1;
 
     protected AssignPoint(int xAxis, int yAxis) {
-        xAxis1 = xAxis;
-        yAxis1 = yAxis;
+       xAxis1 = xAxis;
+       yAxis1 = yAxis;
     }
     protected Object clone() throws CloneNotSupportedException
     {
